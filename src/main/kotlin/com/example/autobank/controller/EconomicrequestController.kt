cd17009d7e9d66ec4d7d0913e87df2ae67fce0b5
Aksel Fosaas <akselsf@stud.ntnu.no>
2024-02-29 15:24:58 +0100

    Fixed responsecodes for create- and getall-economicrequest endpoints

--- src/main/kotlin/com/example/autobank/controller/EconomicrequestController.kt
+++ src/main/kotlin/com/example/autobank/controller/EconomicrequestController.kt
@@ -3,13 +3,10 @@ package com.example.autobank.controller
 import com.example.autobank.data.Economicrequest
 import com.example.autobank.service.EconomicrequestService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/economicrequest")
@@ -19,13 +16,22 @@ class EconomicrequestController() {
     lateinit var economicrequestService: EconomicrequestService;
 
     @PostMapping("/create")
-    fun createEconomicrequest(@RequestBody economicrequest: Economicrequest) {
-        economicrequestService.createEconomicrequest(economicrequest);
+    fun createEconomicrequest(@RequestBody economicrequest: Economicrequest) : ResponseEntity<String> {
+        try {
+            economicrequestService.createEconomicrequest(economicrequest);
+            return ResponseEntity.ok("Economic request created");
+        } catch (e: Exception) {
+            return ResponseEntity.status(500).body("Error creating economic request");
+        }
     }
 
     @GetMapping("/getall")
-    fun getAllEconomicrequests() : List<Economicrequest> {
-        return economicrequestService.getAllEconomicrequests();
+    fun getAllEconomicrequests() : ResponseEntity<List<Economicrequest>> {
+        try {
+            return ResponseEntity.ok(economicrequestService.getAllEconomicrequests());
+        } catch (e: Exception) {
+            return ResponseEntity.status(500).body(emptyList());
+        }
     }
 
 }
--- src/main/kotlin/com/example/autobank/data/Economicrequest.kt
+++ src/main/kotlin/com/example/autobank/data/Economicrequest.kt
@@ -6,31 +6,6 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 
-/*
-@Entity
- class Economicrequest {
-
-    @Id
-    var id: Int = 0
-    var subject: String = ""
-    var purpose: String = ""
-    var date: String = ""
-    var duration: String = ""
-    var description: String = ""
-    var amount: Double = 0.0
-    var personcount: Int = 0
-    var names: String = ""
-    var paymentdescription: String = ""
-    var otherinformation: String = ""
-    var onlineuser_id: Int = 0
-    var status: String = ""
-
-    var processedByUserId: Int = 0
-
-    constructor() {
-    }
-
-}*/
 
 @Entity
 data class Economicrequest(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Int, val subject: String, val purpose: String, val date: String, val duration: String, val description: String, val amount: Double, val personcount: Int, val names: String, val paymentdescription: String, val otherinformation: String, val onlineuser_id: String) {
