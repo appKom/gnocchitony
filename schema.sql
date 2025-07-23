dd4b605df576b523492de648830663c8fe892de8
akselsf <akselsf>
2025-07-23 21:49:53 +0200

    update schema

--- schema.sql
+++ schema.sql
@@ -1,41 +1,43 @@
 CREATE TABLE onlineuser
 (
-    id       INT         NOT NULL PRIMARY KEY IDENTITY (1,1),
+    id       UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
     email    VARCHAR(55) NOT NULL,
     onlineid VARCHAR(55) NOT NULL,
-    fullname VARCHAR(55) NOT NULL
+    fullname VARCHAR(55) NOT NULL,
+    isadmin  BIT         ,
+    lastupdated DATETIME   ,
 );
 
 CREATE TABLE committee
 (
-    id   INT         NOT NULL PRIMARY KEY IDENTITY (1,1),
+    id   UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
     name VARCHAR(55) NOT NULL
 );
 
 CREATE TABLE receipt
 (
-    id            INT            NOT NULL PRIMARY KEY IDENTITY (1,1),
+    id            UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
     amount        DECIMAL(10, 2) NOT NULL,
-    committee_id  INT            NOT NULL references committee (id),
+    committee_id  UNIQUEIDENTIFIER             NOT NULL references committee (id),
     name          VARCHAR(255)   NOT NULL,
     description   VARCHAR(500)   NOT NULL,
     createdat     DATETIME       NOT NULL,
-    onlineuser_id INT            NOT NULL references onlineuser (id),
+    onlineuser_id UNIQUEIDENTIFIER             NOT NULL references onlineuser (id),
     card_number VARCHAR(16) NOT NULL,
     account_number VARCHAR(55) NOT NULL
 );
 
 CREATE TABLE attachment
 (
-    id         INT          NOT NULL PRIMARY KEY IDENTITY (1,1),
-    receipt_id INT          NOT NULL references receipt (id),
+    id        UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    receipt_id UNIQUEIDENTIFIER           NOT NULL references receipt (id),
     name       VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE economicrequest
 (
-    id                 INT            NOT NULL PRIMARY KEY IDENTITY (1,1),
+    id                UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
     subject            VARCHAR(55)    NOT NULL,
     purpose            VARCHAR(500)   NOT NULL,
     date               DATE           NOT NULL,
@@ -47,25 +49,32 @@ CREATE TABLE economicrequest
     paymentdescription varchar(255)   NOT NULL,
     otherinformation   VARCHAR(500)   NOT NULL,
     createdat          DATETIME       NOT NULL,
-    onlineuser_id      INT            NOT NULL references onlineuser (id)
+    onlineuser_id      UNIQUEIDENTIFIER             NOT NULL references onlineuser (id)
 );
 
 CREATE TABLE receiptreview
 (
-    id            INT          NOT NULL PRIMARY KEY IDENTITY (1,1),
-    receipt_id    INT          NOT NULL references receipt (id),
+    id            UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    receipt_id    UNIQUEIDENTIFIER          NOT NULL references receipt (id),
     status        VARCHAR(20)  NOT NULL CHECK (status IN ('APPROVED', 'DENIED')),
     comment       VARCHAR(500) NOT NULL,
     createdat     DATETIME     NOT NULL,
-    onlineuser_id INT          NOT NULL references onlineuser (id)
+    onlineuser_id UNIQUEIDENTIFIER           NOT NULL references onlineuser (id)
 );
 
 CREATE TABLE economicrequestreview
 (
-    id            INT          NOT NULL PRIMARY KEY IDENTITY (1,1),
-    economicrequest_id    INT          NOT NULL references economicrequest (id),
+    id           UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    economicrequest_id    UNIQUEIDENTIFIER          NOT NULL references economicrequest (id),
     status        VARCHAR(20)  NOT NULL CHECK (status IN ('APPROVED', 'DENIED')),
     comment       VARCHAR(500) NOT NULL,
     createdat     DATETIME     NOT NULL,
-    onlineuser_id INT          NOT NULL references onlineuser (id)
+    onlineuser_id UNIQUEIDENTIFIER           NOT NULL references onlineuser (id)
+);
+
+CREATE TABLE economicrequestattachment
+(
+    id                UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    economicrequest_id UNIQUEIDENTIFIER           NOT NULL references economicrequest (id),
+    name              VARCHAR(255) NOT NULL
 );
